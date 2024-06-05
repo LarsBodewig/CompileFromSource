6d844ab5ee2ecf567151644541b68f8106d8e1e7
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2024-06-05 23:19:52 +0200

    Resolve jars with classifier

--- cfs-gradle-plugin-wrapper/src/main/java/dev/bodewig/compilefromsource/gradle/plugin/CompileFromSourcePlugin.java
+++ cfs-gradle-plugin-wrapper/src/main/java/dev/bodewig/compilefromsource/gradle/plugin/CompileFromSourcePlugin.java
@@ -1,8 +1,6 @@
 package dev.bodewig.compilefromsource.gradle.plugin;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
@@ -10,6 +8,8 @@ import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition;
 import org.gradle.api.file.Directory;
+import org.gradle.api.file.DuplicatesStrategy;
+import org.gradle.api.file.FileTree;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.gradle.api.plugins.JavaPlugin;
@@ -28,6 +28,11 @@ public abstract class CompileFromSourcePlugin implements Plugin<Project> {
 	private static final Logger logger = Logging.getLogger(CompileFromSourcePlugin.class);
 
 	/**
+	 * The group of the added tasks
+	 */
+	public static final String TASK_GROUP = "CompileFromSource";
+
+	/**
 	 * The name of the added configuration
 	 */
 	public static final String CONFIGURATION_NAME = "compileFromSource";
@@ -41,6 +46,11 @@ public abstract class CompileFromSourcePlugin implements Plugin<Project> {
 	 * The name of the registered task
 	 */
 	public static final String TASK_NAME_PULL = "pullSources";
+
+	/**
+	 * The name of the registered task
+	 */
+	public static final String TASK_NAME_FILTER = "filterSources";
 
 	/**
 	 * The name of the pulled dir
@@ -79,8 +89,8 @@ public abstract class CompileFromSourcePlugin implements Plugin<Project> {
 								asd.selectArtifact(ArtifactTypeDefinition.JAR_TYPE, null, SOURCES_CLASSIFIER);
 							} else {
 								asd.getRequestedSelectors().forEach(das -> {
-									asd.selectArtifact(das.getType(), das.getExtension(), null);
-									asd.selectArtifact(das.getType(), das.getExtension(), SOURCES_CLASSIFIER);
+									asd.selectArtifact(das.getType(), das.getExtension(), das.getClassifier());
+									asd.selectArtifact(das.getType(), das.getExtension(), SOURCES_CLASSIFIER); // best guess
 								});
 							}
 						});
@@ -100,39 +110,66 @@ public abstract class CompileFromSourcePlugin implements Plugin<Project> {
 		Directory resourcesDir = project.getLayout().getBuildDirectory().dir(RESOURCE_DIR_NAME).get();
 
 		// add task to unpack sources
-		Task pullSources = project.getTasks().create(TASK_NAME_PULL);
-		pullSources.doLast(t -> {
-			// resolve dependencies
-			config.resolve().forEach(f -> {
-				logger.warn("Unpacked " + f.getName());
-				project.copy(cs -> {
-					cs.from(project.zipTree(f));
-					cs.into(pulledDir.dir(f.getName()));
+		Task pullSources = project.getTasks().create(TASK_NAME_PULL, t -> {
+			t.setGroup(TASK_GROUP);
+			t.setDescription("Downloads the configured dependencies and adds them as source directory for compilation");
+			t.doLast(tt -> {
+				// resolve dependencies
+				config.resolve().forEach(f -> {
+					logger.warn("Unpacked " + f.getName());
+					project.copy(cs -> {
+						cs.from(project.zipTree(f));
+						cs.into(pulledDir.dir(f.getName()));
+					});
 				});
+				// split sources and resources
+				for (File f : pulledDir.getAsFile().listFiles()) {
+					project.copy(cs -> {
+						cs.from(f);
+						cs.include("**/*.java");
+						cs.into(sourcesDir);
+						if (!sourcesDir.getAsFileTree().isEmpty()) {
+							cs.filesMatching(sourcesDir.getAsFileTree().getFiles().stream().map(ff -> "**/" + ff.getPath()).toList(), cd -> {
+								logger.warn("Overwriting duplicate file: " + cd.getPath()); // TODO: does not find duplicates!
+							});
+						}
+					});
+					project.copy(cs -> {
+						cs.from(f);
+						cs.exclude("**/*.java");
+						cs.exclude("**/*.class");
+						cs.into(resourcesDir);
+						if (!resourcesDir.getAsFileTree().isEmpty()) {
+							cs.filesMatching(resourcesDir.getAsFileTree().getFiles().stream().map(ff -> "**/" + ff.getPath()).toList(), cd -> {
+								logger.warn("Overwriting duplicate file: " + cd.getPath()); // TODO: does not find duplicates!
+							});
+						}
+					});
+				}
+				// add to source set
+				pulled.getJava().srcDir(sourcesDir);
+				pulled.getResources().srcDir(resourcesDir);
 			});
-			// split sources and resources
-			for (File f : pulledDir.getAsFile().listFiles()) {
-				project.copy(cs -> {
-					cs.from(f);
-					cs.include("**/*.java");
-					cs.into(sourcesDir);
-				});
-				project.copy(cs -> {
-					cs.from(f);
-					cs.exclude("**/*.java");
-					cs.exclude("**/*.class");
-					cs.into(resourcesDir);
-				});
-			}
-			// add to source set
-			pulled.getJava().srcDir(sourcesDir);
-			pulled.getResources().srcDir(resourcesDir);
+		});
+
+		// add task to filter sources
+		Task filterSources = project.getTasks().create(TASK_NAME_FILTER, t -> {
+			t.setGroup(TASK_GROUP);
+			t.setDescription("Delete unwanted files from the pulled dependencies before compiling");
+			t.doLast(tt -> {
+				project.delete(project.fileTree(sourcesDir, ds -> {
+					ds.include("**/META-INF/versions/**/*.java");
+					ds.visit(d -> logger.warn("Deleting: " + d.getFile().getAbsolutePath())); // TODO: prints random folders???
+				}));
+			});
+			t.dependsOn(pullSources); // run after unpacking
 		});
 
 		// run task before compilation
 		TaskCollection<JavaCompile> compileJava = project.getTasks().withType(JavaCompile.class);
 		compileJava.configureEach(t -> {
 			t.dependsOn(pullSources);
+			t.dependsOn(filterSources);
 		});
 	}
 }
